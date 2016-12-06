f3582b439fd474d3a5d6010bd7833130bc73fd08
Kanch <kanchisme@gmail.com>
2016-12-06 13:14:58 +0800

    修改mailService

--- Utilities - Google App Engine/main.py
+++ Utilities - Google App Engine/main.py
@@ -3,6 +3,9 @@ import logging
 
 from flask import Flask
 from flask import render_template
+from datetime import timedelta
+from flask import make_response, request, current_app
+from functools import update_wrapper
 import cuitzhuaqu.zhuaqu as CUIT
 import cuitzhuaqu.MailService as MailService
 import cuitzhuaqu.cacheNews as NewsCache
@@ -10,6 +13,46 @@ import cuitzhuaqu.cacheNews as NewsCache
 
 app = Flask(__name__)
 
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
 
 @app.route('/')
 def hello():
@@ -21,6 +64,7 @@ def newslist():
 
 #下面的都是学术动态，学术预告新闻
 @app.route('/news_xueshu')
+@crossdomain(origin='*')
 def news_xueshu():
     url_xueshu = "http://www.cuit.edu.cn/NewsList?id=4"
     newslistdata = CUIT.getXueshuNews(CUIT.getHtml(url_xueshu))
@@ -36,6 +80,7 @@ def news_xueshu_update():
 
 #下面的都是文化活动，学生社团活动
 @app.route('/news_wenghua')
+@crossdomain(origin='*')
 def news_wenghua():
     url_xueshu = "http://www.cuit.edu.cn/NewsList?id=10"
     newslistdata = CUIT.getXueshuNews(CUIT.getHtml(url_xueshu))
@@ -51,6 +96,7 @@ def news_wenghua_update():
 
 #下面的都是信息公告
 @app.route('/news_xingxigonggao')
+@crossdomain(origin='*')
 def news_xingxigonggao():
     url_xueshu = "http://www.cuit.edu.cn/NewsList?id=2"
     newslistdata = CUIT.getXueshuNews(CUIT.getHtml(url_xueshu))
@@ -66,6 +112,7 @@ def news_xingxigonggao_update():
 
 #下面的都是焦点新闻
 @app.route('/news_jiaodian')
+@crossdomain(origin='*')
 def news_jiaodian():
     url_xueshu = "http://www.cuit.edu.cn/NewsList?id=3"
     newslistdata = CUIT.getXueshuNews(CUIT.getHtml(url_xueshu))
@@ -81,6 +128,7 @@ def news_jiaodian_update():
 
 #下面的都是工作交流
 @app.route('/news_gongzuojiaoliu')
+@crossdomain(origin='*')
 def news_gongzuojiaoliu():
     url_xueshu = "http://www.cuit.edu.cn/NewsList?id=5"
     newslistdata = CUIT.getXueshuNews(CUIT.getHtml(url_xueshu))
@@ -96,6 +144,7 @@ def news_gongzuojiaoliu_update():
 
 #下面的都是综合新闻
 @app.route('/news_zonghe')
+@crossdomain(origin='*')
 def news_zonghe():
     url_xueshu = "http://www.cuit.edu.cn/NewsList?id=1"
     newslistdata = CUIT.getXueshuNews(CUIT.getHtml(url_xueshu))
--- Utilities - OSS/MailService.py
+++ Utilities - OSS/MailService.py
@@ -1,33 +1,33 @@
-﻿#coding:utf-8  #强制使用utf-8编码格式
-import smtplib #加载smtplib模块
+﻿#coding:utf-8  
+import smtplib 
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 from email.utils import formataddr
-my_sender='kanchpybot@163.com' #发件人邮箱账号，为了后面易于维护，所以写成了变量
-my_sender_password = "XYZ2017python"  #发件人邮箱
+my_sender='test@akakanch.com' #发件人邮箱账号，为了后面易于维护，所以写成了变量
+my_sender_password = ""  #发件人邮箱
 
-#This function send mail use kanchpybot@163.com
+#请调用该函数发送邮件
 def SendMail(TO,TITLE,CONTENT):
     ret = Mail(TO,TITLE,CONTENT)
     if ret:
-        print("\tMail sent success!") #如果发送成功则会返回ok，稍等20秒左右就可以收到邮件
+        print("Mail sent success!") #如果发送成功则会返回ok，稍等20秒左右就可以收到邮件
     else:
-        print("\tFailed to sent mail!") #如果发送失败则会返回filed
+        print("Failed to sent mail!") #如果发送失败则会返回filed
 
 def Mail(TO,TITLE,CONTENT):
   ret=True
   my_user = TO
+  msg=MIMEText(CONTENT,'plain','utf-8')
+  msg['From']=formataddr(["成信助手 CUIT Helper",my_sender])  
+  msg['To']=formataddr(["Subscriber",my_user])
+  msg['Subject']="no-reply:" + TITLE + "\t成信助手 | CUIT Helper" #邮件的主题
   try:
-    msg=MIMEText(CONTENT,'plain','utf-8')
-    msg['From']=formataddr(["Kanch's PythonBot @ MyPythonVPS",my_sender])  #括号里的对应发件人邮箱昵称、发件人邮箱账号
-    msg['To']=formataddr(["Autosend by bot",my_user])  #收件人邮箱昵称、收件人邮箱账号
-    msg['Subject']=TITLE #邮件的主题
- 
-    server=smtplib.SMTP("smtp.163.com",25) #发件人邮箱中的SMTP服务器，端口是25
-    server.login(my_sender,my_sender_password)  #括号中对应的是发件人邮箱账号、邮箱密码
-    server.sendmail(my_sender,my_user,msg.as_string())  #括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件
+    server=smtplib.SMTP_SSL("smtp.exmail.qq.com",465) 
+    server.login(my_sender,my_sender_password) 
+    server.sendmail(my_sender,my_user,msg.as_string()) 
     server.quit() 
   except Exception:  
     ret=False
   return ret
  
-#SendMail("1075900121@qq.com",'主题','括号中对应的是发件人邮箱账号、括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件邮箱密码')
+#SendMail("1075900121@qq.com",'测试邮件','括号中对应的是发件人邮箱账号、括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件邮箱密码')
