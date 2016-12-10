b7bdd9daf2afca7859f31c02ab9dcf8174caa4d5
Kanch <kanchisme@gmail.com>
2016-12-10 22:24:48 +0800

    更新所有HTML页面，开始加上更多功能。现在该网站更名字为【成信助手】

--- Web/static/kjslib.js
+++ Web/static/kjslib.js
@@ -49,3 +49,29 @@ function copyToClipBoard(data) {
     var clipBoardContent = data;
     window.clipboardData.setData("Text", clipBoardContent);
 }
+
+function getabout() {
+    var abouttext = "<div style=\"width:55%;margin: 0 auto\">" +
+        "    <div class=\"modal\">" +
+        "        <div class=\"modal-dialog\">" +
+        "            <div class=\"modal-content\">" +
+        "                <div class=\"modal-header\">" +
+        "                    <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-hidden=\"true\" onclick=\"javascript:toggleDiv(\'infodiv\');\">×</button>" +
+        "                    <h4 class=\"modal-title\">关于成信助手-CUIT Helper</h4>" +
+        "                </div>" +
+        "                <div class=\"modal-body\">" +
+        "                    <p>这是一个方便成信学子的网站。目前拥有以下功能：</p>" +
+        "                    <p>成信好老师：从老师的教学风格，严格程度等方面评价老师。你的任何评价都有可能成为日后其它学生选课的依据。</p>" +
+        "                    <p>成信新闻列表：查看我们学校的最新动态，可以订阅新闻更新，这样当有新消息的时候将会通过邮件提醒您。</p>" +
+        "                    <p>成信贴吧大数据（即将上线）：可以对我们学校的贴吧进行一些简单的分析，包括用户维度和贴吧维度的分析。</p>" +
+        "                    <p>目前项目已经开源，欢迎参与贡献：<a href=\"https://github.com/ankanch/cuit-trs\" target=\"_blank\">https://github.com/ankanch/cuit-trs</a></p>" +
+        "                </div>" +
+        "                <div class=\"modal-footer\">" +
+        "                    <button type=\"button\" class=\"btn btn-primary\">by Kanch</button>" +
+        "                </div>" +
+        "            </div>" +
+        "        </div>" +
+        "    </div>" +
+        "</div>";
+    return abouttext;
+}
