98fce996a755757f6cb9a9b0860493e0541b84a4
Kanch <kanchisme@gmail.com>
2017-01-13 16:09:49 +0800

    新增随机显示用户10条帖子的功能

--- Web/bigdataQuery/userFunctions.py
+++ Web/bigdataQuery/userFunctions.py
@@ -1,5 +1,6 @@
 import datasourceconfig.database_settings as DBS
 import pymysql as SQL
+import random
 
 #这个文件是一些用来查找用户，向bigdata插入用户的函数
 def checkUser(username):
@@ -60,3 +61,38 @@ def getBasicInfo(xid):
     if len(result) == 0:
         return [False]
     return result[0]
+
+
+#该函数用于随机抽取10条用户发帖记录
+def getRandom10Post(xid):
+    #获取用户名
+    DBCONN = SQL.connect(host=DBS.HOST_CH, port=3306,user=DBS.USER_CH,passwd=DBS.PASSWORD_CH,db=DBS.NAME_CH,charset='UTF8')
+    DBCONN.set_charset('utf8mb4')
+    DBCUR = DBCONN.cursor()
+    SEL = "SELECT `TIEBAID` FROM `tieba_bigdata` WHERE ID= \"" + xid + "\""
+    DBCUR.execute(SEL)
+    DBCONN.commit()
+    result = DBCUR.fetchall()
+    DBCUR.close()
+    DBCONN.close()
+    if len(result) == 0:
+        return "无发帖信息"
+    DBCONN = SQL.connect(host=DBS.HOST_TB, port=3306,user=DBS.USER_TB,passwd=DBS.PASSWORD_TB,db=DBS.NAME_TB,charset='UTF8')
+    DBCONN.set_charset('utf8mb4')
+    DBCUR = DBCONN.cursor()
+    SEL = "SELECT `CONTENT`,`DATE` FROM `postdata` WHERE AUTHOR= \"" + result[0][0] + "\""
+    DBCUR.execute(SEL)
+    DBCONN.commit()
+    result = DBCUR.fetchall()
+    DBCUR.close()
+    DBCONN.close()
+    i=0
+    rstr = ""
+    HEAD = """ <div class="alert alert-dismissible alert-info">"""
+    TAIL = """</div>"""
+    print(len(result))
+    while i< 10 and i<len(result):
+        rs = str(result[random.randint(0, len(result)-1)][1])  + ": " + str(result[random.randint(0, len(result)-1)][0])
+        i+=1
+        rstr += ( HEAD + rs + TAIL)
+    return rstr
