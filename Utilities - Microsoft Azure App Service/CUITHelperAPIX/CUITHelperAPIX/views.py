16fe73b7f8015fe3f3f95c1286db1ccfb3c8da69
Kanch <kanchisme@gmail.com>
2017-01-22 11:19:55 +0800

    取消API验证

--- Utilities - Microsoft Azure App Service/CUITHelperAPIX/CUITHelperAPIX/views.py
+++ Utilities - Microsoft Azure App Service/CUITHelperAPIX/CUITHelperAPIX/views.py
@@ -40,36 +40,44 @@ def api_statisword(word):
 #       下面是用户数据查询接口
 ############################333
 #获取用户的累计发帖量和最近30天发帖数据
+@app.route('/api/tiebabigdata/postsum/<int:xid>')
 @app.route('/api/tiebabigdata/postsum/<session>/<int:xid>')
-def api_postsum(session,xid):
-    if QueryAPI.checkSession(session) == False:
-        return  "会话无效或已经过期！"
+def api_postsum(xid,session=""):
+    if session != "":
+        if QueryAPI.checkSession(session) == False:
+            return  "会话无效或已经过期！"
     count,count30 = QueryAPI.getTotalPostsSum(xid)
     return str(count) + "," + str(count30)
 
 #获取活跃时间段图数据
+@app.route('/api/tiebabigdata/activitytimezon/<int:xid>')
 @app.route('/api/tiebabigdata/activitytimezon/<session>/<int:xid>')
-def api_activitytimezone(session,xid):
-    if QueryAPI.checkSession(session) == False:
-        return  "会话无效或已经过期！"
+def api_activitytimezone(xid,session=""):
+    if session != "":
+        if QueryAPI.checkSession(session) == False:
+            return  "会话无效或已经过期！"
     listt =  QueryAPI.getActivityTimeZone(xid)
     return str(listt)
 
 
 #获取最近活跃度图
+@app.route('/api/tiebabigdata/activitytimeline/<int:xid>/<int:days>')
 @app.route('/api/tiebabigdata/activitytimeline/<session>/<int:xid>/<int:days>')
-def api_activitytimeline(session,xid,days):
-    if QueryAPI.checkSession(session) == False:
-        return  "会话无效或已经过期！"
+def api_activitytimeline(xid,days,session=""):
+    if session != "":
+        if QueryAPI.checkSession(session) == False:
+            return  "会话无效或已经过期！"
     listt =  QueryAPI.getActivityTimeLine(xid,days)
     return str(listt)
 
 #获取用户关系图数据
+@app.route('/api/tiebabigdata/relationcircle/<int:xid>')
 @app.route('/api/tiebabigdata/relationcircle/<session>/<int:xid>')
 @as_json
-def api_relationcircle(session,xid):
-    if QueryAPI.checkSession(session) == False:
-        return  "会话无效或已经过期！"
+def api_relationcircle(xid,session=""):
+    if session != "":
+        if QueryAPI.checkSession(session) == False:
+            return  "会话无效或已经过期！"
     listt =  QueryAPI.getReadlationCircle(xid)
     i=0
     iljson = []
@@ -80,16 +88,22 @@ def api_relationcircle(session,xid):
     return dict(label=listt[0],value=iljson)
 
 #获取常用关键字图
+@app.route('/api/tiebabigdata/keymap/<int:xid>')
 @app.route('/api/tiebabigdata/keymap/<session>/<int:xid>')
-def api_keymap(session,xid):
-    if QueryAPI.checkSession(session) == False:
-        return  "会话无效或已经过期！"
+def api_keymap(xid,session=""):
+    if session != "":
+        if QueryAPI.checkSession(session) == False:
+            return  "会话无效或已经过期！"
     listt =  QueryAPI.getKeymap(xid)
     return str(listt)
 
 #获取用户标签
+@app.route('/api/tiebabigdata/gettags/<int:xid>')
 @app.route('/api/tiebabigdata/gettags/<session>/<int:xid>')
-def api_gettags(session,xid):
+def api_gettags(xid,session=""):
+    if session != "":
+        if QueryAPI.checkSession(session) == False:
+            return  "会话无效或已经过期！"
     return "这是一个测试页面，显示API的用法"
 ############################333
 #       下面是邮箱验证接口
